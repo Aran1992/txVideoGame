e1af99704e6891f77fe61776e8b7ab6a829b06d4
Aran1992 <2572261667@qq.com>
2019-09-24 21:57:04 +0800

    修改配置

--- resource/config/pack.py
+++
@@ -1,9 +0,0 @@
-import os
-import zipfile
-
-path = './config'
-zipf = zipfile.ZipFile('config.bin', 'w')
-for file in os.listdir(path):
-    file_path = os.path.join(path, file)
-    zipf.write(file_path, file)
-zipf.close()
