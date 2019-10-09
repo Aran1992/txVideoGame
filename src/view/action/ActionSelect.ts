1938f07a99de45eded8cd3c68f2e95254cb93c8a
Aran1992 <2572261667@qq.com>
2019-10-09 16:26:34 +0800

    修改视频跳转的判定

--- src/view/action/ActionSelect.ts
+++ src/view/action/ActionSelect.ts
@@ -2,7 +2,6 @@ class ActionSelect extends ActionTimerSceneBase {
     private ITEM_COUNT: number = 5;
     private answerID: number;
     private isSelected: boolean;
-    private checkLocked: boolean = false;
     private exitTimer: number;
 
     public exit() {
@@ -22,26 +21,12 @@ class ActionSelect extends ActionTimerSceneBase {
 
     protected onInit(): void {
         super.onInit();
-        GameDispatcher.getInstance().addEventListener(GameEvent.BUY_HAOGAN, this.onBuySuccessCallback, this);
         this.answerID = this.model.moren;
         for (let i: number = 0; i < this.ITEM_COUNT; i++) {
             const button: eui.Component = this[`button${i}`];
             button.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onClickButton, this);
             button["index"] = i;
             this[`selected${i}`].visible = false;
-        }
-        const func = GameCommon.getInstance().getLockedOptionIDs[this.model.id];
-        if (func) {
-            this.checkLocked = true;
-            for (let i: number = 0; i < this.ITEM_COUNT; i++) {
-                this[`timeImg${i}`].visible = false;
-                this[`suo${i}`].visible = false;
-            }
-            const lockedIDs = func() || [];
-            lockedIDs.forEach(id => {
-                this['timeImg' + id].visible = true;
-                this['suo' + id].visible = true;
-            });
         }
     }
 
@@ -75,23 +60,12 @@ class ActionSelect extends ActionTimerSceneBase {
         const button = event.currentTarget;
         const index = button["index"];
         this.answerID = index + 1;
-        if (this.checkLocked && this['timeImg' + index].visible) {
-            VideoManager.getInstance().videoPause();
-            PromptPanel.getInstance().onShowBuyHaoGan(index);
-        } else {
-            this.isSelected = true;
-            this[`selected${index}`].visible = true;
-            for (let i: number = 0; i < this.ITEM_COUNT; i++) {
-                this[`button${i}`].touchEnabled = false;
-            }
-            this.exitTimer = egret.setTimeout(() => this.onBackSuccess(), this, 1000);
+        this.isSelected = true;
+        this[`selected${index}`].visible = true;
+        for (let i: number = 0; i < this.ITEM_COUNT; i++) {
+            this[`button${i}`].touchEnabled = false;
         }
-    }
-
-    private onBuySuccessCallback() {
-        let index = this.answerID - 1;
-        this['timeImg' + index].visible = false;
-        this['suo' + index].visible = false;
+        this.exitTimer = egret.setTimeout(() => this.onBackSuccess(), this, 1000);
     }
 
     private onExit() {
