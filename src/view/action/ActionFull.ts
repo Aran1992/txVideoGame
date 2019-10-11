67e2f0b7959dd8dc3deeaa553c8fd8fb2344052f
Aran1992 <2572261667@qq.com>
2019-10-11 17:23:00 +0800

    修改偷听

--- src/view/action/ActionFull.ts
+++ src/view/action/ActionFull.ts
@@ -1,20 +1,10 @@
 class ActionFull extends ActionSceneBase {
     private guide_left: eui.Image;
     private guide_right: eui.Image;
-    // private tipsImg:eui.Image;
     private timeBar: eui.ProgressBar;
     private moveUp: boolean;
     private start_posX: number;
     private start_posX1: number;
-
-    // private updateResize() {
-    // 	this.width = size.width;
-    // 	this.height = size.height;
-
-    // }
-    public onSuccessItem(idx: number) {
-
-    }
 
     protected onSkinName(): void {
         this.skinName = skins.ActionFullSkin;
@@ -23,17 +13,13 @@ class ActionFull extends ActionSceneBase {
     protected onInit(): void {
         super.onInit();
         this.updateResize();
-        let count = 2;
         this.moveUp = false;
         this.guide_left.y = this.height / 2;
         this.guide_right.y = this.height / 2;
-        // this.tipsImg.anchorOffsetX = this.tipsImg.width/2;
-        // this.tipsImg.x = this.guide_left.x+((this.guide_right.x-this.guide_left.x)/2);
         this.start_posX = this.guide_left.x;
         this.start_posX1 = this.guide_right.x;
-        // this.guide_left.addEventListener(egret.Event.ENTER_FRAME, this.onFrame, this);
         GameDispatcher.getInstance().addEventListener(GameEvent.VIDEO_FULL_END, this.onVideo_Full_End, this);
-        var hdCfg: Modelhudong = JsonModelManager.instance.getModelhudong()[this.model.type];
+        const hdCfg: Modelhudong = JsonModelManager.instance.getModelhudong()[this.model.type];
         if (hdCfg && hdCfg.des)
             this.timeBar['desc'].text = hdCfg.des;
         this.timeBar.maximum = this.maxTime;
@@ -48,24 +34,6 @@ class ActionFull extends ActionSceneBase {
     }
 
     private onVideo_Full_End() {
-        // this.guide_left.removeEventListener(egret.Event.ENTER_FRAME, this.onFrame, this);
         this.exit();
-    }
-
-    private onFrame(): void {
-        if (!this.moveUp) {
-            this.guide_left.x -= 2;
-            this.guide_right.x += 2;
-            if (this.guide_left.x < this.start_posX) {
-                this.moveUp = true;
-            }
-
-        } else {
-            this.guide_left.x += 2;
-            this.guide_right.x -= 2;
-            if (this.guide_left.x > this.start_posX + 30) {
-                this.moveUp = false;
-            }
-        }
     }
 }
--- src/view/action/ActionListen.ts
+++ src/view/action/ActionListen.ts
@@ -1,10 +1,8 @@
-class ActionListen extends ActionSceneBase {
-    private DRINK_MAX: number = 4;
+class ActionListen extends ActionTimerSceneBase {
+    private ITEM_COUNT: number = 4;
     private ansId: number;
-    private isSelected: boolean;
-    private timeBar1: eui.ProgressBar;
-    private timeBar2: eui.ProgressBar;
-    private desc: eui.Label;
+    private selected: boolean;
+    private timer: number;
 
     public exit() {
         this.stopRun();
@@ -18,70 +16,65 @@ class ActionListen extends ActionSceneBase {
     protected onInit(): void {
         super.onInit();
         this.updateResize();
-        this.timeBar1.maximum = this.maxTime;
-        this.timeBar1.slideDuration = 0;
-        this.timeBar1.value = this.maxTime;
-        this.timeBar2.slideDuration = 0;
-        this.timeBar2.maximum = this.maxTime;
-        this.timeBar2.value = this.maxTime;
-        this.initTimeInfo();
-        this.ansId = this.model.moren;
-        for (let i: number = 0; i < this.DRINK_MAX; i++) {
-            let checkBtn: eui.RadioButton = (this[`drink${i}_btn`] as eui.RadioButton);
-            let rate_X: number = checkBtn.x / GameDefine.GAME_VIEW_WIDTH;
-            let rate_y: number = checkBtn.y / GameDefine.GAME_VIEW_HEIGHT;
-            checkBtn.x = Math.floor(size.width * rate_X);
-            checkBtn.y = Math.floor(size.height * rate_y);
-            checkBtn.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onSelectDrink, this);
+        this.ansId = this.model.moren + 3;
+        for (let i: number = 0; i < this.ITEM_COUNT; i++) {
+            this[`listen${i}`].addEventListener(egret.TouchEvent.TOUCH_TAP, this.onClickListen, this);
+            this[`listen${i}`].visible = true;
+            this[`listen${i}`].value = i;
+            this[`enter${i}`].addEventListener(egret.TouchEvent.TOUCH_TAP, this.onClickEnter, this);
+            this[`enter${i}`].visible = false;
+            this[`enter${i}`].value = i;
         }
         GameDispatcher.getInstance().addEventListener(GameEvent.VIDEO_FULL_END, this.exit, this);
     }
 
-    protected update(dt): void {
-        super.update(dt);
-        this.timeBar1.value = this.runTime;
-        this.timeBar2.value = this.runTime;
-    }
-
-    private initTimeInfo() {
-        const hdCfg: Modelhudong = JsonModelManager.instance.getModelhudong()[this.model.type];
-        if (hdCfg && hdCfg.des) {
-            this.desc.text = hdCfg.des;
+    private onClickListen(event: egret.Event): void {
+        if (this.selected) {
+            return;
         }
-    }
-
-    private onSelectDrink(event: egret.Event): void {
-        if (!this.isSelected) {
-            this.isSelected = true;
-            const button = (event.currentTarget as eui.RadioButton);
-            this.ansId = parseInt(button.value);
-            for (let i: number = 0; i < this.DRINK_MAX; i++) {
-                (this[`drink${i}_btn`] as eui.RadioButton).touchEnabled = false;
-            }
-            SoundManager.getInstance().playSound(`music${this.ansId}.mp3`);
-            const DURATION = 100;
-            button.enabled = true;
-            egret.Tween.get(button)
+        const listenBtn = event.currentTarget;
+        this.selected = true;
+        const name = `music${listenBtn.value + 1}.mp3`;
+        SoundManager.getInstance().playSound(name);
+        this.timer = egret.setTimeout(() => {
+            const DURATION = 200;
+            egret.Tween.get(listenBtn)
                 .to({alpha: 0}, DURATION)
-                .to({alpha: 1}, DURATION)
-                .call(() => button.enabled = false)
-                .wait(10000)
                 .call(() => {
-                    GameDispatcher.getInstance().dispatchEvent(
-                        new egret.Event(GameEvent.VIDEO_FULL_END),
-                        (this.ansId + 3).toString()
-                    );
+                    listenBtn.visible = false;
+                    const enterBtn = this[`enter${listenBtn.value}`];
+                    enterBtn.visible = true;
+                    enterBtn.alpha = 0;
+                    egret.Tween.get(enterBtn)
+                        .to({alpha: 1}, DURATION)
+                        .call(() => this.selected = false);
                 });
+        }, this, SoundManager.getInstance().getMusicLength(name));
+    }
+
+    private onClickEnter(event: egret.Event) {
+        if (this.selected) {
+            return;
         }
+        const DURATION = 1000;
+        const enterBtn = event.currentTarget;
+        this.ansId = enterBtn.value + 4;
+        this.selected = true;
+        egret.Tween.get(enterBtn)
+            .to({alpha: 0}, DURATION)
+            .call(() => {
+                GameDispatcher.getInstance().dispatchEvent(
+                    new egret.Event(GameEvent.VIDEO_FULL_END),
+                    this.ansId.toString()
+                );
+            });
     }
 
     private onExit() {
         if (this.parent) {
             this.parent.removeChild(this);
         }
-        for (let i: number = 0; i < this.DRINK_MAX; i++) {
-            (this[`drink${i}_btn`] as eui.RadioButton).removeEventListener(egret.TouchEvent.TOUCH_TAP, this.onSelectDrink, this);
-        }
+        egret.clearTimeout(this.timer);
         GuideManager.getInstance().isGuide = false;
         GuideManager.getInstance().curState = false;
     }
