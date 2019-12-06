f5d112e1a7425d09c0b39a28595b6b529f1cba47
chenfuxing748 <380032013@qq.com>
2019-12-06 11:29:22 +0800

    时间修改

--- src/Platform.ts
+++ src/Platform.ts
@@ -118,7 +118,7 @@ class DebugPlatform implements Platform {
 
     //获得上线时间，其它时间可以此时间上叠加
     public getSaleBeginTime() {
-        return 1575043200 + DebugPlatform.s_offsetTime;//2019/11/30 12:0:0
+        return 1578585600 + DebugPlatform.s_offsetTime;//2020/1/10 12:0:0
     }
 
     public getPlatform() {
