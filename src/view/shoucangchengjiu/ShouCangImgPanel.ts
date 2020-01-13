dad2aff29ba28804d1eef5846e42f425fcc75231
Aran1992 <2572261667@qq.com>
2020-01-13 17:22:38 +0800

    修复设置壁纸的BUG

--- src/view/shoucangchengjiu/ShouCangImgPanel.ts
+++ src/view/shoucangchengjiu/ShouCangImgPanel.ts
@@ -1,41 +1,24 @@
 class ShouCangImgPanel extends eui.Component {
-    private mainGroup: eui.Group;
     private bgBtn: eui.Group;
     private imgGroup: eui.Group;
-    private yindaoGroup: eui.Group;
-    private left_jiantou: eui.Image;
-    private right_jiantou: eui.Image;
-    private yindaoGroup1: eui.Group;
     private btnSetXinDong: eui.Button;
     private playSound: eui.Button;
     private datu: eui.Image;
     private info: Modelshoucang;
     private btnShare: eui.Button;
-    private moveUp: boolean;
-    private start_posX: number;
     private starPos: number = 0;
-    private isOne: boolean = false;
     private animRecords: SCImageData[];
-    private readonly imgMaxNumb: number = 5;
+    private readonly imgMaxNum: number;
     private curImg;
     private imgSound: string[] = [];
+    private imgIdx: number = 0;
 
     constructor(data) {
         super();
         this.info = data;
-        this.imgMaxNumb = Math.min(Number(this.info.src), 5);
+        this.imgMaxNum = Math.min(Number(this.info.src), 5);
         this.once(egret.Event.COMPLETE, this.onLoadComplete, this);
         this.once(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
-    }
-
-    private _imgIndx: number = 0;
-
-    private get imgIndx() {
-        return this._imgIndx;
-    }
-
-    private set imgIndx(m) {
-        this._imgIndx = m;
     }
 
     protected onRegist(): void {
@@ -61,7 +44,7 @@ class ShouCangImgPanel extends eui.Component {
 
     protected onInit(): void {
         this.onInitImg();
-        for (let i = 0; i < this.imgMaxNumb; i++) {
+        for (let i = 0; i < this.imgMaxNum; i++) {
             this['xindong' + i].visible = UserInfo.main_Img == this['img' + i].source;
         }
     }
@@ -76,48 +59,25 @@ class ShouCangImgPanel extends eui.Component {
 
     private onSetXinDong() {
         SoundManager.getInstance().playSound("ope_click.mp3");
-        for (let i = 0; i < this.imgMaxNumb; i++) {
+        for (let i = 0; i < this.imgMaxNum; i++) {
             this['xindong' + i].visible = false;
         }
-        let idx = this.imgIndx + 1;
-        if (idx > 4) {
-            idx = 0;
-        }
-        this['xindong' + idx].visible = true;
-        UserInfo.main_Img = this['img' + idx].source;
+        this['xindong' + this.imgIdx].visible = true;
+        UserInfo.main_Img = this['img' + this.imgIdx].source;
         GameDispatcher.getInstance().dispatchEvent(new egret.Event(GameEvent.MAIN_IMG_REFRESH));
         if (UserInfo.curBokData)
             UserInfo.curBokData.main_Img = UserInfo.main_Img;
         GameCommon.getInstance().setBookData(FILE_TYPE.AUTO_FILE);
     }
 
-    private onFrame() {
-        if (!this.moveUp) {
-            this.right_jiantou.x += 2;
-            this.left_jiantou.x -= 2;
-            if (this.left_jiantou.x < this.start_posX) {
-                this.moveUp = true;
-            }
-        } else {
-            this.right_jiantou.x -= 2;
-            this.left_jiantou.x += 2;
-            if (this.left_jiantou.x > this.start_posX + 20) {
-                this.moveUp = false;
-            }
-        }
-    }
-
     private onInitImg() {
         this.animRecords = [];
-        let _record: SCImageData;
-        let img: eui.Group;
-        for (let i = 0; i < this.imgMaxNumb; i++) {
-            img = (this[`img${i}Group`] as eui.Group);
-            _record = new SCImageData(img.x, img.y, img.scaleX, img.width, img.height, img.parent.getChildIndex(img));
+        for (let i = 0; i < this.imgMaxNum; i++) {
+            let img: eui.Group = this[`img${i}Group`] as eui.Group;
             img.name = i + '';
-            this.animRecords.push(_record);
+            this.animRecords.push(new SCImageData(img.x, img.y, img.scaleX, img.width, img.height, img.parent.getChildIndex(img)));
         }
-        this.imgIndx = 0;
+        this.imgIdx = 0;
     }
 
     private onClose() {
@@ -126,20 +86,15 @@ class ShouCangImgPanel extends eui.Component {
         GameDispatcher.getInstance().dispatchEvent(new egret.Event(GameEvent.CLOSE_VIEW), 'ShouCangImgPanel')
     }
 
+    private onClickImg(event: egret.Event) {
+        this.curImg = event.currentTarget;
+    }
+
     private onEventDown(event: egret.TouchEvent) {
         this.starPos = event.stageX;
     }
 
     private onEventEnd(event: egret.TouchEvent) {
-        if (this.yindaoGroup.visible) {
-            this.imgGroup.touchEnabled = false;
-            this.imgGroup.touchChildren = false;
-            this.yindaoGroup.removeEventListener(egret.Event.ENTER_FRAME, this.onFrame, this);
-            this.yindaoGroup.visible = false;
-            this.yindaoGroup1.visible = true;
-            GuideManager.getInstance().onShowImg(this.mainGroup, this.bgBtn, 'leftClose');
-            return;
-        }
         if (this.starPos == event.stageX && this.curImg) {
             this.datu.source = this.curImg.source;
             if (this.curImg.width != size.width) {
@@ -151,7 +106,6 @@ class ShouCangImgPanel extends eui.Component {
                 tw.to({width: GameDefine.GAME_VIEW_WIDTH * scale, height: GameDefine.GAME_VIEW_HEIGHT * scale}, 100);
                 return;
             }
-
         }
         if (this.starPos > event.stageX) {
             if (this.starPos - event.stageX > 30) {
@@ -160,7 +114,6 @@ class ShouCangImgPanel extends eui.Component {
         } else if (this.starPos < event.stageX) {
             if (event.stageX - this.starPos > 30) {
                 this.onLastImg();
-
             }
         }
         this.curImg = null;
@@ -174,66 +127,61 @@ class ShouCangImgPanel extends eui.Component {
     }
 
     private onPlaySound() {
-        let idx = this.imgIndx;
         SoundManager.getInstance().stopMusicAll();
-        if (idx + 1 >= this.imgMaxNumb)
-            idx = 0;
-        else
-            idx = idx + 1;
-        if (this.imgSound[idx]) {
-            SoundManager.getInstance().playSound(this.imgSound[idx], null, true);
-        }
+        SoundManager.getInstance().playSound(this.imgSound[this.imgIdx], null, true);
     }
 
     private onNextImg() {
-        if (this.imgIndx + 1 >= this.imgMaxNumb)
-            this.imgIndx = 0;
-        else
-            this.imgIndx = this.imgIndx + 1;
-        let idx = this.imgIndx;
-        SoundManager.getInstance().stopMusicAll();
-        if (idx + 1 >= this.imgMaxNumb)
-            idx = 0;
-        else
-            idx = idx + 1;
-        if (this.imgSound[idx]) {
-            SoundManager.getInstance().playSound(this.imgSound[idx], null, true);
-        }
         this.play();
-    }
-
-    private onClickImg(event: egret.Event) {
-        this.curImg = event.currentTarget;
-    }
-
-    private play(): void {
-        let currIndex: number = this.imgIndx;
-        let offset: number = 1;
-        let recordInfo: SCImageData;
-        let param = this.imgMaxNumb + currIndex;
-        let infySlot: eui.Group;
-        for (let j = currIndex; j < param; j++) {
-            infySlot = (this[`img${j >= this.imgMaxNumb ? j - this.imgMaxNumb : j}Group`] as eui.Group);
-            infySlot.name = (j - currIndex - offset >= 0 ? j - currIndex - offset : this.imgMaxNumb - offset - (j - currIndex)) + '';
-            recordInfo = this.animRecords[Number(infySlot.name)];
-            egret.Tween.get(infySlot).to({}, 100).to({
-                x: recordInfo.posX,
-                scaleX: recordInfo.scale,
-                scaleY: recordInfo.scale,
-                y: recordInfo.posY,
-                width: recordInfo.width,
-                height: recordInfo.height
-            }, 150, egret.Ease.sineIn);
-            infySlot.parent.setChildIndex(infySlot, recordInfo.childNum);
+        this.imgIdx++;
+        if (this.imgIdx >= this.imgMaxNum) {
+            this.imgIdx = 0;
         }
+        SoundManager.getInstance().stopMusicAll();
+        SoundManager.getInstance().playSound(this.imgSound[this.imgIdx], null, true);
     }
 
     private onLastImg() {
-        if (this.imgIndx - 1 < 0)
-            this.imgIndx = 4;
-        else
-            this.imgIndx = this.imgIndx - 1;
-        this.play();
+        this.play(true);
+        this.imgIdx--;
+        if (this.imgIdx < 0) {
+            this.imgIdx = this.imgMaxNum - 1;
+        }
+        SoundManager.getInstance().stopMusicAll();
+        SoundManager.getInstance().playSound(this.imgSound[this.imgIdx], null, true);
+    }
+
+    private play(reverse = false): void {
+        for (let i = 0; i < this.imgMaxNum; i++) {
+            let imgGroup: eui.Group = this[`img${i}Group`];
+            let j = i - this.imgIdx;
+            if (j < 0) {
+                j = this.imgMaxNum + j;
+            }
+            if (reverse) {
+                j++;
+                if (j >= this.imgMaxNum) {
+                    j = 0;
+                }
+            } else {
+                j--;
+                if (j < 0) {
+                    j = this.imgMaxNum - 1;
+                }
+            }
+            imgGroup.name = j + "";
+            let info = this.animRecords[imgGroup.name];
+            egret.Tween.get(imgGroup)
+                .to({
+                    x: info.posX,
+                    scaleX: info.scale,
+                    scaleY: info.scale,
+                    y: info.posY,
+                    width: info.width,
+                    height: info.height
+                }, 150, egret.Ease.sineIn);
+            imgGroup.parent.setChildIndex(imgGroup, info.childNum);
+        }
     }
 
     private updateResize() {
@@ -243,7 +191,7 @@ class ShouCangImgPanel extends eui.Component {
 
     private getImgArray() {
         let imgs: string[] = [];
-        let count = Math.min(this.imgMaxNumb, Number(this.info.src));
+        let count = Math.min(this.imgMaxNum, Number(this.info.src));
         for (let i = 1; i <= count; i++) {
             imgs.push(this.info.id + "_" + String(i) + "_jpg");
         }
@@ -253,73 +201,52 @@ class ShouCangImgPanel extends eui.Component {
     private onLoadComplete(): void {
         this.updateResize();
         this.touchEnabled = false;
-        let num = this.width / 2 - this[`img${0}Group`].width / 2;
-        this.isOne = true;
+        let x = this.width / 2 - this[`img0Group`].width / 2;
 
         let imgs: string[] = this.getImgArray();
-        let miaoshus: string[];
-        miaoshus = this.info.kuozhan.split(";");
-
+        let miaoshus: string[] = this.info.kuozhan.split(";");
         for (let j = 0; j < imgs.length; j++) {
             this[`xindong${j}`].visible = false;
+            this['img' + j].source = imgs[j];
+            this['img' + j].width = 1006;
+            this['img' + j].height = 537;
+            this[`img${j}Group`].scaleX = 0.9;
+            this[`img${j}Group`].scaleY = 0.9;
             switch (j) {
                 case 0:
-                    this[`img${j}Group`].x = num;
+                    this[`img${j}Group`].x = x;
                     this[`img${j}Group`].scaleX = 1;
                     this[`img${j}Group`].scaleY = 1;
                     break;
                 case 4:
-                    this[`img${j}Group`].scaleX = 0.9;
-                    this[`img${j}Group`].scaleY = 0.9;
                     this[`img${j}Group`].x = 0 - (750);
                     break;
                 case 3:
-                    this[`img${j}Group`].scaleX = 0.9;
-                    this[`img${j}Group`].scaleY = 0.9;
                     this[`img${j}Group`].x = 0 - (1062) - 350;
                     break;
                 case 1:
-                    this[`img${j}Group`].scaleX = 0.9;
-                    this[`img${j}Group`].scaleY = 0.9;
-                    this[`img${j}Group`].x = num + (1200);
+                    this[`img${j}Group`].x = x + (1200);
                     break;
                 case 2:
-                    this[`img${j}Group`].scaleX = 0.9;
-                    this[`img${j}Group`].scaleY = 0.9;
-                    this[`img${j}Group`].x = num + (1150) + 350;
+                    this[`img${j}Group`].x = x + (1150) + 350;
                     break;
             }
             this['miaoshu' + j].text = miaoshus[j];
             if (!miaoshus[j] || miaoshus[j] == '') {
                 this['shengyin' + j].visible = false;
             }
-            this['img' + j].source = imgs[j];
             this['img' + j].touchEnabled = true;
             this['img' + j].name = j + '';
             this['img' + j].addEventListener(egret.TouchEvent.TOUCH_END, this.onClickImg, this);
-            this['img' + j].width = 1006;
-            this['img' + j].height = 537;
         }
-        let sounds: string[] = [];
         let count = Number(this.info.shengyin) || 0;
         for (let i = 1; i <= count; i++) {
-            sounds.push("resource/assets/shopimages/" + this.info.id + "/" + this.info.id + "_" + i + ".mp3");
+            this.imgSound.push("resource/assets/shopimages/" + this.info.id + "/" + this.info.id + "_" + i + ".mp3");
         }
-
-        this.imgSound = sounds;
-        this.yindaoGroup.visible = false;
         this.onInit();
-        this.imgIndx = 4;
-        let idx = this.imgIndx;
+        this.imgIdx = 0;
         SoundManager.getInstance().stopMusicAll();
-        if (idx + 1 >= this.imgMaxNumb)
-            idx = 0;
-        else
-            idx = idx + 1;
-        if (this.imgSound[idx]) {
-            SoundManager.getInstance().playSound(this.imgSound[idx], null, true)
-        }
-        this.play();
+        SoundManager.getInstance().playSound(this.imgSound[this.imgIdx], null, true);
         this.onRegist();
         this.updateResize();
         this.datu.visible = false;
@@ -328,11 +255,7 @@ class ShouCangImgPanel extends eui.Component {
     private onShare(): void {
         SoundManager.getInstance().playSound("ope_click.mp3");
         let imgs: string[] = this.getImgArray();
-        let idx = this.imgIndx + 1;
-        if (idx == this.imgMaxNumb) {
-            idx = 0;
-        }
-        let image_src: string = imgs[idx];
+        let image_src: string = imgs[this.imgIdx];
         if (!image_src) return;
         let texture: egret.Texture = RES.getRes(image_src);
         if (texture) {
